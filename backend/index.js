202c50816f2c27aa48c7a0ad7d654a8f5d7f7d70
vinnietito <vincentnjuguna69@gmail.com>
2025-09-16 10:26:33 +0300

    Commits

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
@@ -9,10 +10,10 @@ app.use(bodyParser.json());
 
 // ✅ DB Connection
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "V@38080135k", 
-  database: "greenlife"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME,
 });
 db.connect(err => {
   if (err) throw err;
