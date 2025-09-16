8a32434c832e6c22a7e373ef7a89c9ef419b0d94
vinnietito <vincentnjuguna69@gmail.com>
2025-09-16 09:08:29 +0300

    Commits

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+//COnnecting to the database
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "V@38080135kim",
+    database: "greenlife"
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log("MySQL Connected...")
+});
+
+//Create user table if not exist
+db.query(`
+    CREATE TABLE IF NOT EXIST users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    points INT DEFAULT 0
+    )
+    `);
+
+//Register
+app.post("/api/register", (req, res) => {
+    const { name, email, password } = req.body;
+    db.query(
+        
+    )
+})
